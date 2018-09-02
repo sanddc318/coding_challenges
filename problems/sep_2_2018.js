ca52375cda71c453748f1b47637f1fed3ed27e0f
Dontavious Sanders <sanddc178@gmail.com>
2018-09-02 17:59:27 -0500

    Solved first challenege using recursion

--- problems/sep_2_2018.js
+++ problems/sep_2_2018.js
@@ -1,17 +1,25 @@
-// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+/* Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+*/
 
+// function FirstFactorial(num) {
+//   for (let i = num - 1; i >= 1; --i) {
+//     num *= i;
+//   }
+
+//   return num;
+// }
+
+// Using recursion.
 function FirstFactorial(num) {
-  // code goes here
-  for (let i = num - 1; i >= 1; --i) {
-    num *= i;
-  }
+  if (num <= 0) return 1;
 
-  return num;
+  return num * FirstFactorial(num - 1);
 }
 
-FirstFactorial(readline());
+// Do not change code below this line.
+FirstFactorial(generateRandNum());
 
-function readline() {
+function generateRandNum() {
   const randNum = Math.ceil(Math.random() * 18);
   return randNum;
 }

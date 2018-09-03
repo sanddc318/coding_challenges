91428c5d9a400281fccc4b18d4257a1b79b070bf
Dontavious Sanders <sanddc178@gmail.com>
2018-09-03 16:04:35 -0500

    Found challenge for day2

---
+++ problems/sep_3_2018.js
@@ -0,0 +1,15 @@
+/*
+  Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the   string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+
+  Test cases:
+  "Argument goes here" => "Argument"
+  "fun&!! times" => "times"
+  "123456789 1234" => "123456789"
+*/
+
+function LongestWord(sen) {
+  // code goes here
+  return sen;
+}
+
+LongestWord("Argument goes here");

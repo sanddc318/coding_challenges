f285cc45a41d9195f636abdc90db0a4ba7633038
Dontavious Sanders <sanddc178@gmail.com>
2018-09-05 10:57:34 -0500

    Found challenge for day4.

---
+++ problems/sep_5_2018.js
@@ -0,0 +1,11 @@
+// Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm:
+
+// Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+
+function LetterChanges(str) {
+  // code goes here
+  return str
+}
+
+// Do not change code below this line.
+LetterChanges(readline())
